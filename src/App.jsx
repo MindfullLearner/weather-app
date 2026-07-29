5479a42fec71726896091f997101392cd2fd6b37
NayabMaryam <nayabmaryam40@gmail.com>
2026-07-29 15:40:24 +0500

    feat: Seac functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
+import { useState } from "react";
 
 function App() {
+   const [searchCity, setSearchCity] = useState("");
+   const [weatherCity, setWeatherCity] = useState("Karachi");
+   function handleSearch() 
+   {
+    setWeatherCity(searchCity);
+   }
   return (
     <main className="app">
       <h1>Weather App</h1>
       <p>Search for a city to see the weather.</p>
 
-      <SearchBar/>
+      <SearchBar
+        searchCity={searchCity}
+        setSearchCity={setSearchCity}
+        handleSearch={handleSearch}
+      />
 
       <WeatherCard 
-      city="Karachi"
+      city={weatherCity}
       temperature={34}
       condition="Clear Sky"
       humidity={52}
       wind={12}/>
     </main>
   );
+  
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,17 @@
 import "./SearchBar.css";
-function SearchBar() {
+
+
+function SearchBar({ searchCity, setSearchCity, handleSearch }) {
+  
   return (
     <div className="search-bar">
       <input
         type="text"
         placeholder="Enter city name"
+        value={searchCity}
+        onChange={(e) => setSearchCity(e.target.value)}
       />
-      <button>Search</button>
+      <button onClick={handleSearch}>Search</button>
     </div>
   );
 }
